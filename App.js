c17a3b56cb7ea5a1c05fc29bbfd820fd07e5b1e0
Snehal Shinde <mandhares827@gmail.com>
2023-12-08 07:47:01 +0530

    Component inside component i.e. component composit

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title =  (
+const Title = () => (
     <div id="title"><h1 id="h1tag">This is my title JSX!</h1></div>
 );
 const HeadingComponent = () => (
      <div id="container">
-        {Title}
+        <Title/>
         <h1 id="heading">This is my first JSX!</h1>
     </div>
 );
